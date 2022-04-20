762b524b93000027707fd40850e7ec3b8661bba8
arif-projects <arifulhaque60332@gmail.com>
2022-04-20 19:19:25 +0600

    myworkupdate

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,44 +1,51 @@
-import React from 'react';
-import './Banner.css'
-import { Col, Container, Row } from 'react-bootstrap';
-import { Typewriter } from 'react-simple-typewriter';
-import 'react-simple-typewriter/dist/index.js';
+import React from "react";
+import "./Banner.css";
+import { Col, Container, Row } from "react-bootstrap";
+import { Typewriter } from "react-simple-typewriter";
+import "react-simple-typewriter/dist/index.js";
 const Banner = () => {
-    return (
-        <div id = "home" className = "banner mt-5">
-            <Container>
-            <Row>
-                <Col className = "d-flex align-items-center" md={6}>
-                    <div className="banner-text text-start">
-                        <h3>Hey there!,</h3>
-                        <h1>I'm <span className="skillHeader">Mohammad Arif</span></h1>
-                        <h3 className="aniText">
-                            Front end
-                            <span className="last">
-                                <Typewriter
-                                loop
-                                cursor
-                                cursorStyle="_"
-                                typeSpeed={[70]}
-                                deleteSpeed={[50]}
-                                delaySpeed={[1000]}
-                                words={[' web developer']}
-                                />
-                            </span> 
-                            
-                        </h3>
-                       <button className = "banner-button m-3"><a href ="#contact">Contact me</a></button>
-                    </div>
-                </Col>
-                <Col md={6}>
-                    <div className="banner-image">
-                        <img className = "w-75" src={'https://i.ibb.co/pr27Y6Y/my-bg2-1.png'} alt="" />
-                    </div>
-                </Col>
-            </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div id="home" className="banner mt-5">
+      <Container>
+        <Row>
+          <Col className="d-flex align-items-center" md={6}>
+            <div className="banner-text text-start">
+              <h3>Hey there!,</h3>
+              <h1>
+                I'm <span className="skillHeader">Mohammad Arif</span>
+              </h1>
+              <h3 className="aniText">
+                Front end
+                <span className="last">
+                  <Typewriter
+                    loop
+                    cursor
+                    cursorStyle="_"
+                    typeSpeed={[70]}
+                    deleteSpeed={[50]}
+                    delaySpeed={[1000]}
+                    words={[" web developer"]}
+                  />
+                </span>
+              </h3>
+              <button className="banner-button m-3">
+                <a href="#contact">Contact me</a>
+              </button>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div className="banner-image">
+              <img
+                className="w-75"
+                src={"https://i.ibb.co/pr27Y6Y/my-bg2-1.png"}
+                alt=""
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Pages/MyWork/MyWork.js
+++ src/Pages/MyWork/MyWork.js
@@ -1,112 +1,158 @@
-import React from 'react';
-import './MyWork.css';
-import { Col, Container, Row } from 'react-bootstrap';
-import { Typewriter } from 'react-simple-typewriter';
-import 'react-simple-typewriter/dist/index.js';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
-import { useEffect } from 'react';
+import React from "react";
+import "./MyWork.css";
+import { Col, Container, Row } from "react-bootstrap";
+import { Typewriter } from "react-simple-typewriter";
+import "react-simple-typewriter/dist/index.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const MyWork = () => {
-    useEffect(()=>{
-        AOS.init({duration: 2000});
-    },[]);
-    return (
-        <div id = "work" className = "my-5 mywork">
-                        <h1 className="aniText mb-5">
-                            My
-                            <span className="last">
-                                <Typewriter
-                                loop
-                                cursor
-                                cursorStyle="_"
-                                typeSpeed={[100]}
-                                deleteSpeed={[70]}
-                                delaySpeed={[1000]}
-                                words={[' work']}
-                                />
-                            </span> 
-                            
-                        </h1>
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+  return (
+    <div id="work" className="my-5 mywork">
+      <h1 className="aniText mb-5">
+        My
+        <span className="last">
+          <Typewriter
+            loop
+            cursor
+            cursorStyle="_"
+            typeSpeed={[100]}
+            deleteSpeed={[70]}
+            delaySpeed={[1000]}
+            words={[" work"]}
+          />
+        </span>
+      </h1>
 
-<Container>
-            <Row>
+      <Container>
+        <Row className="align-items-center">
+          <Col md={4}>
+            <div data-aos="fade-right" className="cardd mb-3">
+              <div className="description">
+                <img
+                  className="w-100 h-50"
+                  src={"https://i.ibb.co/tMq5vn3/work4.png"}
+                  alt=""
+                />
+                <h3>Act Shady</h3>
+                <p>
+                  This is Niche web app. Where users can login & buy sunglasses.
+                </p>
+              </div>
 
-                <Col md={4}>
-                    <div data-aos="fade-right" className="cardd mb-3">
-                        <div className="description">
-                        <img className = "w-100 h-50" src={'https://i.ibb.co/tMq5vn3/work4.png'} alt="" />
-                        <h3>Act Shady</h3>
-                         <p>This is Niche web app. Where users can login & buy sunglasses.</p>
-                        </div>
-                       
-                        <button className = "workBtn mt-3"><a href="#home">Watch Project</a></button>
-                       
-                       
-                    </div>
-                </Col>
-                <Col md={4}>
-                    <div data-aos="flip-up" className="cardd mb-3">
-                      <div className="description">
-                        <img className = "w-100 h-50" src={'https://i.ibb.co/v3RcRMm/5.png'} alt="" />
-                   
-                        <h3>Island Hoppers</h3>
-                         <p>This is a Travel website. Users can log in to my webpage & Purchese offerings.</p>
-                       </div>
-                         <button className = "workBtn mt-3"><a href="#home">Watch Project</a></button>
-                    </div>
-                </Col>
-                <Col md={4}>
-                    <div data-aos="fade-left" className="cardd mb-3">
-                    <div className="description">
-                        <img className = "w-100 h-50" src={'https://i.ibb.co/vhVqkLT/work-2.png'} alt="" />
-                          <h3>Time Sheriff</h3>
-                         <p>It is an online watch store. People can see our collection of watch &
-                         Also login & make purchases online. People can also see our offerings.</p>
-                         </div>
-                         <button className = "workBtn mt-3"><a href="#home">Watch Project</a></button>
-                    </div>
-                  
-                </Col>
-                <Col md={4}>
-                    <div data-aos="fade-right" className="cardd mb-3">
-                    <div className="description">
-                        <img className = "w-100 h-50" src={'https://i.ibb.co/Zh86rkm/doctor.png'} alt="" />
-                          <h3>Life Motion Clinic</h3>
-                         <p>It is a Hospital website.
-                            Technology: React,Firebase,React
-                            Bootstrap HTML, CSS.</p>
-                         </div>
-                         <button className = "workBtn mt-3"><a href="https://life-motion-clinic.netlify.app/home">Watch Project</a></button>
-                    </div>
-                  
-                </Col>
-                <Col md={4}>
-                    <div data-aos="fade-up" className="cardd mb-3">
-                    <div className="description">
-                        <img className = "w-100 h-50" src={'https://i.ibb.co/wKJ9cXw/teacher.png'} alt="" />
-                          <h3>Zoom online Couching</h3>
-                         <p>It is an online Couching Center.Student can Choose teacher & learn online.</p>
-                         </div>
-                         <button className = "workBtn mt-3"><a href="#home">Watch Project</a></button>
-                    </div>
-                  
-                </Col>
-                <Col md={4}>
-                    <div data-aos="fade-left" className="cardd mb-3">
-                    <div className="description">
-                        <img className = "w-100 h-50" src={'https://i.ibb.co/RBY7WgX/book.png'} alt="" />
-                          <h3>Book Motion</h3>
-                         <p>It is an online Library. People read book online</p>
-                         </div>
-                         <button className = "workBtn mt-3"><a href="https://book-motion.netlify.app/">Watch Project</a></button>
-                    </div>
-                  
-                </Col>
-            </Row>
-            </Container>
-        </div>
-    );
+              <button className="workBtn mt-3">
+                <a href="https://act-shady.web.app/">Watch Project</a>
+              </button>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div data-aos="flip-up" className="cardd mb-3">
+              <div className="description">
+                <img
+                  className="w-100 h-50"
+                  src={"https://i.ibb.co/v3RcRMm/5.png"}
+                  alt=""
+                />
+
+                <h3>Island Hoppers</h3>
+                <p>
+                  This is a Travel website. Users can log in to my webpage &
+                  Purchese offerings.
+                </p>
+              </div>
+              <button className="workBtn mt-3">
+                <a href="https://island-hoppers.web.app/home#home">
+                  Watch Project
+                </a>
+              </button>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div data-aos="fade-left" className="cardd mb-3">
+              <div className="description">
+                <img
+                  className="w-100 h-50"
+                  src={"https://i.ibb.co/vhVqkLT/work-2.png"}
+                  alt=""
+                />
+                <h3>Time Sheriff</h3>
+                <p>
+                  It is an online watch store. People can see our collection of
+                  watch & Also login & make purchases online. People can also
+                  see our offerings.
+                </p>
+              </div>
+              <button className="workBtn mt-3">
+                <a href="https://time-sheriff.web.app/home">Watch Project</a>
+              </button>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div data-aos="fade-right" className="cardd mb-3">
+              <div className="description">
+                <img
+                  className="w-100 h-50"
+                  src={"https://i.ibb.co/Zh86rkm/doctor.png"}
+                  alt=""
+                />
+                <h3>Life Motion Clinic</h3>
+                <p>
+                  It is a Hospital website. Technology: React,Firebase,React
+                  Bootstrap HTML, CSS.
+                </p>
+              </div>
+              <button className="workBtn mt-3">
+                <a href="https://life-motion-clinic.netlify.app/home">
+                  Watch Project
+                </a>
+              </button>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div data-aos="fade-up" className="cardd mb-3">
+              <div className="description">
+                <img
+                  className="w-100 h-50"
+                  src={"https://i.ibb.co/wKJ9cXw/teacher.png"}
+                  alt=""
+                />
+                <h3>Zoom online Couching</h3>
+                <p>
+                  It is an online Couching Center.Student can Choose teacher &
+                  learn online.
+                </p>
+              </div>
+              <button className="workBtn mt-3">
+                <a href="https://zoom-couching-center.netlify.app/">
+                  Watch Project
+                </a>
+              </button>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div data-aos="fade-left" className="cardd mb-3">
+              <div className="description">
+                <img
+                  className="w-100 h-50"
+                  src={"https://i.ibb.co/RBY7WgX/book.png"}
+                  alt=""
+                />
+                <h3>Book Motion</h3>
+                <p>It is an online Library. People read book online</p>
+              </div>
+              <button className="workBtn mt-3">
+                <a href="https://book-motion.netlify.app/">Watch Project</a>
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default MyWork;
